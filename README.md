14fae858001278440d90fb80e5c76a27b907e3be
jaschutte <jaschutte49@gmail.com>
2022-11-17 12:33:37 +0100

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arduino Libraries for Croaker.
+None of the code belongs to us. This repository is purely here for us to install with ease.
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in libs/* ; do
+	if [ -d "$i" ]; then
+		cd "$i" # We're inside the folder now
+		rm build
+		cmake -B build
+		cmake --build build
+		cmake --install build
+		cd "../.." # We're back at the root
+	fi
+done
